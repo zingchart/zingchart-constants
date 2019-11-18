51bb37f8f03f8429b114582ac8bb8ed88103d8e8
Nick Ardecky <nardecky@zingsoft.com>
2019-11-18 13:18:57 -0800

    [feature] Add markers to constants

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import EVENT_NAMES from './events.js';
 import METHOD_NAMES from './methods.js';
+import MARKER_NAMES from './markers.js';
 import MISC from './misc.js';
 
 const {DEFAULT_WIDTH, DEFAULT_HEIGHT} = MISC;
@@ -7,6 +8,7 @@ const {DEFAULT_WIDTH, DEFAULT_HEIGHT} = MISC;
 export default {
   EVENT_NAMES,
   METHOD_NAMES,
+  MARKER_NAMES,
   DEFAULT_WIDTH,
   DEFAULT_HEIGHT
 };
