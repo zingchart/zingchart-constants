688ed3c40ef4319865b148b947b2d472ba1aff0f
jeanettephung <jeanettephung@hotmail.com>
2023-08-21 09:06:44 -0700

    Add type definition file

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare const EVENT_NAMES: string[];
+declare const METHOD_NAMES: string[];
+declare const MARKER_NAMES: string[];
+declare const DEFAULT_WIDTH: string;
+declare const DEFAULT_HEIGHT: number;
+declare const DEFAULT_OUTPUT: string;
+
+export default {
+  EVENT_NAMES,
+  METHOD_NAMES,
+  MARKER_NAMES,
+  DEFAULT_WIDTH,
+  DEFAULT_HEIGHT,
+  DEFAULT_OUTPUT,
+};
